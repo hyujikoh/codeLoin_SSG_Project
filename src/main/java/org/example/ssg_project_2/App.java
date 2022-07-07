43c9f45f002165ff90f2d68c91a730447be18c41
hyujikoh <dvum0045@gmail.com>
2022-07-07 15:05:52 +0900

    gitignore 수정

--- src/main/java/org/example/ssg_project_2/App.java
+++ src/main/java/org/example/ssg_project_2/App.java
@@ -1,3 +1,5 @@
+package org.example.ssg_project_2;
+
 import java.util.Scanner;
 
 public class App {
@@ -5,7 +7,7 @@ public class App {
             System.out.println("== 명언 SSG ==");
 
             Scanner sc = new Scanner(System.in);
-
+            int i = 1;
             outer:
             while (true) {
                 System.out.printf("명령) ");
@@ -14,6 +16,13 @@ public class App {
                 switch (cmd) {
                     case "종료":
                         break outer;
+                    case "등록":
+                        System.out.printf("명언) ");
+                        String desc = sc.nextLine().trim();
+                        System.out.printf("작가) ");
+                        String author = sc.nextLine().trim();
+                        System.out.println(i+"번 게시물이 등록되었다");
+                        i++;
                 }
             }
 
--- src/main/java/org/example/ssg_project_2/main.java
+++ src/main/java/org/example/ssg_project_2/main.java
@@ -1,3 +1,7 @@
+package org.example.ssg_project_2;
+
+import org.example.ssg_project_2.App;
+
 public class main {
     public static void main(String[] args) {
         new App().run();
